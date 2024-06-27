6b79021a94bcf5cfc42f18e4855683e2f54550bc
Ghenciu <sebyghenciu5@gmail.com>
2024-06-27 23:51:54 +0300

    Retus la pag - My account

--- src/app/pages/admin/account/account.component.ts
+++ src/app/pages/admin/account/account.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatDialogModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
